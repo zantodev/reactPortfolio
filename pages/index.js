f75f7b9c0a826cada08eb6fb44670c802970bd07
Tony Zepeda <mbp@Tonys-MacBook-Pro.local>
2022-11-18 19:17:56 -0800

    Third Commit

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/newlogo.svg" />
       </Head>
     <Main />
     <About />
