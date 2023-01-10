6aba07e8c377b9254aece796c3ae7d897cff1277
Tony Zepeda <61566056+zantodev@users.noreply.github.com>
2023-01-10 14:44:47 -0800

    updated layout

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
   return (
     <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
-        <p className="text-3xl tracking-widest uppercase text-[#5651e5]">
+        <p className="text-3xl tracking-widest py-16 uppercase text-[#5651e5]">
           What I've Built
         </p>
         {/* <h2 className="py-4 text-2xl">What I've Built</h2> */}
